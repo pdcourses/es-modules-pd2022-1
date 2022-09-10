b5e4016b01da9b89a2c54f2020dfc8c0ad49c843
pdcourses <freshitpt@gmail.com>
2022-09-10 11:42:39 +0300

    es modules . structure

---
+++ index.js
@@ -0,0 +1,9 @@
+import pow2 from './math.js';
+
+const result = pow2(2);
+console.log(result);
+
+console.log(pow2(5));
+console.log(pow2(6));
+console.log(pow2(7));
+console.log(pow2(8));
---
+++ math.js
@@ -0,0 +1,14 @@
+const x = 100;
+
+const pow2 = (base) => base ** 2;
+
+export default pow2;  // public access
+
+/*
+class Math{
+    public:
+        function pow2
+    private:
+    protected:
+}
+*/
