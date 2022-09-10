9ab9534d8fd75294c42ea01daec1151b2c170023
pdcourses <freshitpt@gmail.com>
2022-09-10 12:08:47 +0300

    abracadabra or smth about default export

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-import math from './math.js';
+import abracadabra from './math.js';
 
-console.log(math);
+console.log(abracadabra);
 
-const res1 = math.pow2(2);
+
+const res1 = abracadabra.pow2(2);
 console.log(res1);
 
-const res2 = math.sum(1,2);
+const res2 = abracadabra.sum(1,2);
 console.log(res2);
 
 
-const res3 = math.sum(100,250);
+const res3 = abracadabra.sum(100,250);
 console.log(res3);
--- math.js
+++ math.js
@@ -5,3 +5,4 @@ const pow2 = (base) => base ** 2;
 const sum = (a,b) => a+b;
 
 export default {pow2, sum}; 
+//export default 'hello! it is es modules :)';
